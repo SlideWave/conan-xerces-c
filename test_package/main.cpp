f021845b119113abcd2fe100d83c70f128a1250f
Cinder Roxley <cinder@sdf.org>
2016-09-29 12:46:50 -0500

    Buildable test program

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,5 +1,15 @@
+#include <xercesc/util/PlatformUtils.hpp>
 
+using namespace xercesc;
 
 int main(void) {
+	try {
+		XMLPlatformUtils::Initialize();
+	}
+	catch (const XMLException&) {
+		return 1;
+	}
 
+	XMLPlatformUtils::Terminate();
+	return 0;
 }
